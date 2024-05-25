40e260cb56a790e6a2830cc04646939e721f8877
rainstriker <cloneltorrer@gmail.com>
2024-05-26 00:08:29 +0700

    format

--- client/src/lib/components/board-tab/BoardTab.svelte
+++ client/src/lib/components/board-tab/BoardTab.svelte
@@ -41,18 +41,18 @@
 </script>
 
 <div
-	class="border-b border-gray-200 text-center text-sm font-medium text-gray-500 dark:border-gray-700 dark:text-gray-400"
+	class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400"
 >
-	<ul class="-mb-px flex flex-wrap">
+	<ul class="flex flex-wrap -mb-px">
 		{#each boards as board}
 			<Tab activate={board.id === target.id} {board} {onSelect} {onDelete} />
 		{/each}
 		<a
 			href={null}
-			class="inline-block cursor-pointer rounded-t-lg border-b-2 border-transparent p-4 hover:border-gray-300 hover:text-gray-600 dark:hover:text-gray-300"
+			class="inline-block p-4 border-b-2 border-transparent rounded-t-lg cursor-pointer hover:border-gray-300 hover:text-gray-600 dark:hover:text-gray-300"
 			on:click={() => addBoard()}
 		>
-			<PlusOutline class="h-4 w-4" />
+			<PlusOutline class="w-4 h-4" />
 		</a>
 	</ul>
 </div>
