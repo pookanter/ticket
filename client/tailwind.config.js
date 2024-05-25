812e062bf62ee5e09945600db81ec00c54575c7c
rainstriker <cloneltorrer@gmail.com>
2024-05-25 14:20:18 +0700

    ➕ Update prettier and tailwindcss dependencies

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}',  "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': {
+          '50': '#eff4ff',
+          '100': '#dbe6fe',
+          '200': '#bfd3fe',
+          '300': '#93b4fd',
+          '400': '#6090fa',
+          '500': '#3b76f6',
+          '600': '#2563eb',
+          '700': '#1d58d8',
+          '800': '#1e4baf',
+          '900': '#1e408a',
+          '950': '#172a54',
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'),
+  ],
 }
 
