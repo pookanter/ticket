148e9a5485f5c7f25161ad0aba002c2ee0b8f573
rainstriker <cloneltorrer@gmail.com>
2024-05-27 00:38:27 +0700

    organize folder

--- migration/migrate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Name of the Docker service running MySQL
-SERVICE_NAME="mysql"
-
-# Execute the mysql command in the Docker container to import the schema.sql file
-docker-compose exec -T $SERVICE_NAME mysql -u $MYSQL_ROOT_USERNAME -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/schema.sql
