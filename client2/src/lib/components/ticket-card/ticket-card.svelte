7ab41f8e27a3310a3feeb9158d769af8553bf2eb
rainstriker <cloneltorrer@gmail.com>
2024-05-31 01:24:55 +0700

    dialog ui

---
+++ client2/src/lib/components/ticket-card/ticket-card.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card/index';
+	import * as Dialog from '$lib/components/ui/dialog/index';
+	import { EditOutline, PlusOutline } from 'flowbite-svelte-icons';
+	interface Ticket {
+		id: number;
+		title: string;
+		description: string;
+	}
+
+	export let ticket: Ticket = {
+		id: 0,
+		title: '',
+		description: ''
+	};
+	export let edit = (ticket: Ticket) => {};
+</script>
+
+<Card.Root>
+	<Card.Header class="group">
+		<Card.Title>
+			<div class="relative flex start">
+				<span>{ticket.title}</span>
+				<button
+					class="absolute top-0 right-0 flex items-center justify-center invisible p-1 -mt-4 -mr-4 rounded cursor-pointer group-hover:visible hover:text-accent-foreground hover:bg-accent"
+					on:click={() => edit(ticket)}
+				>
+					<EditOutline class="size-4" />
+				</button>
+			</div>
+		</Card.Title>
+	</Card.Header>
+</Card.Root>
--- client2/src/routes/app/+page.svelte
+++ client2/src/routes/app/+page.svelte
@@ -10,6 +10,8 @@
 	import { flip } from 'svelte/animate';
 	import { dndzone } from 'svelte-dnd-action';
 	import Button from '$lib/components/ui/button/button.svelte';
+	import TicketCard from '$lib/components/ticket-card/ticket-card.svelte';
+	import * as Dialog from '$lib/components/ui/dialog/index';
 	interface Ticket {
 		id: number;
 		title: string;
@@ -100,70 +102,98 @@
 		boards[index].statuses[colIdx].tickets = e.detail.items;
 		boards[index].statuses = [...boards[index].statuses];
 	}
+
+	enum Resource {
+		Board = 'board',
+		Status = 'status',
+		Ticket = 'ticket'
+	}
+	enum Method {
+		Create = 'create',
+		Update = 'update'
+	}
+
+	const dialogState = {
+		open: false,
+		resrc_type: Resource.Board,
+		method: Method.Create
+	};
+
+	function editTicket(ticket: Ticket) {
+		console.log('editTicket', ticket);
+		dialogState.resrc_type = Resource.Ticket;
+		dialogState.open = true;
+	}
 </script>
 
 <section class="h-[90vh] w-full">
-	<div class="h-full mt-2">
-		<BoardTabs.Root>
-			<BoardTabs.List>
-				{#each boards as board (board.id)}
-					<BoardTabs.Trigger value={`${boards[index].id}`}>
-						<span>{boards[index].title}</span>
-					</BoardTabs.Trigger>
-				{/each}
-				<div
-					class="flex items-center justify-center p-1 m-3 rounded cursor-pointer hover:text-accent-foreground hover:bg-accent"
-				>
-					<PlusOutline class="size-4" />
-				</div>
-			</BoardTabs.List>
-			{#each boards as board (board.id)}
-				<BoardTabs.Content value={`${boards[index].id}`}>
+	<Dialog.Root open={dialogState.open}>
+		<div class="h-full mt-2">
+			<BoardTabs.Root>
+				<BoardTabs.List>
+					{#each boards as board (board.id)}
+						<BoardTabs.Trigger value={`${boards[index].id}`}>
+							<span>{boards[index].title}</span>
+						</BoardTabs.Trigger>
+					{/each}
 					<div
-						class="flex justify-start h-full gap-4 p-4 overflow-x-auto overflow-y-hidden"
-						use:dndzone={{
-							items: boards[index].statuses,
-							flipDurationMs,
-							type: 'columns',
-							dropTargetStyle: {}
-						}}
-						on:consider={handleDndConsiderColumns}
-						on:finalize={handleDndFinalizeColumns}
+						class="flex items-center justify-center p-1 m-3 rounded cursor-pointer hover:text-accent-foreground hover:bg-accent"
 					>
-						{#each boards[index].statuses as status (status.id)}
-							<div class="relative" animate:flip={{ duration: flipDurationMs }}>
-								<Card.Root class="px-2 w-80">
-									<Card.Header>
-										<Card.Title>{status.title}</Card.Title>
-									</Card.Header>
-									<Card.Content class="px-0">
-										<div
-											class="flex flex-col gap-2 min-h-32"
-											use:dndzone={{ items: status.tickets, flipDurationMs, dropTargetStyle: {} }}
-											on:consider={(e) => handleDndConsiderCards(status.id, e)}
-											on:finalize={(e) => handleDndFinalizeCards(status.id, e)}
-										>
-											{#each status.tickets as ticket (ticket.id)}
-												<div animate:flip={{ duration: flipDurationMs }}>
-													<Card.Root>
-														<Card.Header>
-															<Card.Title>{ticket.title}</Card.Title>
-														</Card.Header>
-														<Card.Content>{ticket.id}</Card.Content>
-													</Card.Root>
-												</div>
-											{/each}
-										</div>
-										<Button variant="outline" class="w-full mt-2">
-											<PlusOutline class="size-4" />
-										</Button>
-									</Card.Content>
-								</Card.Root>
-							</div>
-						{/each}
+						<PlusOutline class="size-4" />
 					</div>
-				</BoardTabs.Content>
-			{/each}
-		</BoardTabs.Root>
-	</div>
+				</BoardTabs.List>
+				{#each boards as board (board.id)}
+					<BoardTabs.Content value={`${boards[index].id}`}>
+						<div
+							class="flex justify-start h-full gap-4 p-4 overflow-x-auto overflow-y-hidden"
+							use:dndzone={{
+								items: boards[index].statuses,
+								flipDurationMs,
+								type: 'columns',
+								dropTargetStyle: {}
+							}}
+							on:consider={handleDndConsiderColumns}
+							on:finalize={handleDndFinalizeColumns}
+						>
+							{#each boards[index].statuses as status (status.id)}
+								<div class="relative" animate:flip={{ duration: flipDurationMs }}>
+									<Card.Root class="px-2 w-80">
+										<Card.Header>
+											<Card.Title>{status.title}</Card.Title>
+										</Card.Header>
+										<Card.Content class="px-0">
+											<div
+												class="flex flex-col gap-2 min-h-32"
+												use:dndzone={{ items: status.tickets, flipDurationMs, dropTargetStyle: {} }}
+												on:consider={(e) => handleDndConsiderCards(status.id, e)}
+												on:finalize={(e) => handleDndFinalizeCards(status.id, e)}
+											>
+												{#each status.tickets as ticket (ticket.id)}
+													<div animate:flip={{ duration: flipDurationMs }}>
+														<TicketCard {ticket} edit={editTicket} />
+													</div>
+												{/each}
+											</div>
+											<Button variant="outline" class="w-full mt-2">
+												<PlusOutline class="size-4" />
+											</Button>
+										</Card.Content>
+									</Card.Root>
+								</div>
+							{/each}
+						</div>
+					</BoardTabs.Content>
+				{/each}
+			</BoardTabs.Root>
+		</div>
+		<Dialog.Content>
+			<Dialog.Header>
+				<Dialog.Title>Are you sure absolutely sure?</Dialog.Title>
+				<Dialog.Description>
+					This action cannot be undone. This will permanently delete your account and remove your
+					data from our servers.
+				</Dialog.Description>
+			</Dialog.Header>
+		</Dialog.Content>
+	</Dialog.Root>
 </section>
