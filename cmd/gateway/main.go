d31ab9ab8d5c5eb4d00ee390fc299087b8185478
rainstriker <cloneltorrer@gmail.com>
2024-05-31 17:46:38 +0700

    fi import

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -8,8 +8,8 @@ import (
 	"ticket/config"
 	"ticket/pkg/apikit"
 
-	"github.com/labstack/echo/middleware"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/valyala/fasthttp"
 )
 
